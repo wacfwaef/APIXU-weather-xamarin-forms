fde0c7ff22d7beee1c5d7b703d833ae66679494f
up201809185 <up201809185@fe.up.pt>
2019-05-30 16:01:49 +0100

    place button and cityname on same line

--- sun_or_rain.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ sun_or_rain.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "d4319caf-7a4f-4563-aee4-d0111decdc14",
+		"XAMARIN_BUILD_ID", "1bdcd0b9-6a32-4fb8-a2a9-cd1627428c14",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
