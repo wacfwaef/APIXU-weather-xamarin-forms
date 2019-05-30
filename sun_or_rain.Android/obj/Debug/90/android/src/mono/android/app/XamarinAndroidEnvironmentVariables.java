79e447449057a4fdaeef9c891a329041ed20e997
up201809185 <up201809185@fe.up.pt>
2019-05-30 16:08:10 +0100

    interactivity issues fix

--- sun_or_rain.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ sun_or_rain.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "1bdcd0b9-6a32-4fb8-a2a9-cd1627428c14",
+		"XAMARIN_BUILD_ID", "cbda457f-c42d-4bbe-ab1a-9f83606c5d79",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
